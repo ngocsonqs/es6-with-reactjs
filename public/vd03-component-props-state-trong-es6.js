ce09a4a82f62e80f9700d2b78e0102750f1ef53f
ngocsonqs <ngocson_jp@yahoo.co.jp>
2017-11-07 00:30:11 +0900

    component props state

--- public/vd03-component-props-state-trong-es6.js
+++ public/vd03-component-props-state-trong-es6.js
@@ -5,13 +5,29 @@ class Note extends React.Component {
         this.state = {
             studentNum : 10
         }
+
+        // cach 3 :
+        this.addNewStudent = this.addNewStudent.bind(this)
     }
+
+    addNewStudent() {
+        this.setState({
+            studentNum : this.state.studentNum + 1
+        })
+    }
+
     render() {
         return (
             <div>
                 <p>{this.props.txt}</p>
                 <p>Student Num : {this.state.studentNum}</p>
                 <button>Delete</button>
+                <button onClick ={ () => {
+                    this.setState({studentNum : this.state.studentNum + 1})
+                }}>Add student - Cach 1</button>
+
+                <button onClick = { this.addNewStudent.bind(this)}>Add student - Cach 2</button>
+                <button onClick = { this.addNewStudent }>Add student - Cach 3</button>
             </div>
         );
     }
